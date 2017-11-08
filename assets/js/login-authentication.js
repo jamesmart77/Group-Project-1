a504e4cf1eeefca1ef7ef2da5132460bd896c393
Joseph A Chingas <JChingas87@Josephs-MacBook-Pro.local>
2017-11-07 21:17:43 -0500

    links changes

--- assets/js/login-authentication.js
+++ assets/js/login-authentication.js
@@ -70,12 +70,12 @@ $(document).ready(function() {
             if (window.location.href.indexOf(("home-page.html") == -1)) {
                 window.location.href = "https://ehulseman.github.io/Group-Project-1/home-page.html";
             }
-            if (window.location.href.indexOf(("saved-page.html") == -1)) {
-                window.location.href = "https://ehulseman.github.io/Group-Project-1/home-page.html";
-            }
-            if (window.location.href.indexOf(("results-page.html") == -1)) {
-                window.location.href = "https://ehulseman.github.io/Group-Project-1/home-page.html";
-            }
+            // if (window.location.href.indexOf(("saved-page.html") == -1)) {
+            //     window.location.href = "https://ehulseman.github.io/Group-Project-1/home-page.html";
+            // }
+            // if (window.location.href.indexOf(("results-page.html") == -1)) {
+            //     window.location.href = "https://ehulseman.github.io/Group-Project-1/home-page.html";
+            // }
         } else {
             console.log('not logged in');
             // btnLogout.hide();
