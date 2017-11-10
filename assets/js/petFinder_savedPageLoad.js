31f19507ba5e5c99876b1a23605d25821593731d
Joseph A Chingas <JChingas87@Josephs-MacBook-Pro.local>
2017-11-09 21:33:11 -0500

    saved-page.html and css updates

--- assets/js/petFinder_savedPageLoad.js
+++ assets/js/petFinder_savedPageLoad.js
@@ -47,8 +47,45 @@ function getSavedPetIDs(petsArr) {
 
                 var petfinder = data.petfinder.pet;
 
-                console.log(petfinder)
-                    // var isArray = $.isArray(petfinder.pet)
+                var infoDiv = $("<div id='animal-div' class='card col-lg-2 col-md-4 col-sm-12 col-xs-12'>")
+                var cardBody = $("<div class='card-body'>")
+                var name = $("<h4 class='card-title'>" + petfinder.name.$t + "</h4>");
+                // var animal = $("<div>").html("<b>Animal Type: </b>" + petfinder.animal['$t']);
+                // var age = $("<div>").html("<b>Age: </b>" + petfinder.age['$t']);
+                // var sex = $("<div>").html("<b>Sex: </b>" + petfinder.sex['$t']);
+                // var size = $("<div>").html("<b>Size: </b>" + petfinder.size['$t']);
+                var imgAddress = "";
+
+                for (var t = 0; t < petfinder.media.photos.photo.length; t++) {
+                    let imgItr = petfinder.media.photos.photo[t].$t;
+
+                    if (imgItr.indexOf(300) >= 0) {
+                        imgAddress = petfinder.media.photos.photo[t].$t;
+                    }
+
+                }
+
+                if (imgAddress === "") {
+                    imgAddress = "assets/images/imageNA.png"
+                }
+
+                var photo = $("<img class='card-img-top'>").attr({
+                    src: imgAddress,
+                    alt: 'pet image',
+                    'data-id': petfinder.id.$t
+                });
+                // infoDiv.append(animal)
+                // infoDiv.append(name)
+                // infoDiv.append(age)
+                // infoDiv.append(sex)
+                // infoDiv.append(size)
+                infoDiv.append(photo)
+                infoDiv.append(cardBody)
+                cardBody.append(name)
+
+                // return infoHTML;
+                $('#saved-animal-container').append(infoDiv);
+                // var isArray = $.isArray(petfinder.pet)
 
                 // //isArray will check if more than one animal has been returned
                 // if (isArray) {
