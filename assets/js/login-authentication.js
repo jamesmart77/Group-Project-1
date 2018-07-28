97cc3d43dee848381f221f562af38e4d6b1914f2
James Martineau <James.Martineau@libertymutual.com>
2018-07-27 23:32:45 -0400

    firebase auth updates

--- assets/js/login-authentication.js
+++ assets/js/login-authentication.js
@@ -57,13 +57,19 @@ $(document).ready(() => {
         // TODO: Check for real email
         const email = txtEmail.val().trim();
         const pass = txtPassword.val().trim();
-        const auth = firebase.auth();
         if ((email == "") || (pass == "")) {
+            $("#login-error").show();
             return false;
         }
         //Sign Up
-        const promise = auth.createUserWithEmailAndPassword(email, pass);
-        promise.catch(e => console.log(e.message));
+        firebase.auth().createUserWithEmailAndPassword(email, pass).catch((error) => {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            console.log("ERROR:\n")
+            console.log("code: ", errorCode)
+            console.log("message: ", errorMessage)
+          });
     });
 
     // signout
@@ -98,14 +104,14 @@ function OAuthSignIn() {
     var provider = new firebase.auth.GoogleAuthProvider();
 
     console.log("button")
-    firebase.auth().signInWithPopup(provider).then(function(result) {
+    firebase.auth().signInWithPopup(provider).then(function (result) {
         // This gives you a Google Access Token. You can use it to access the Google API.
         var token = result.credential.accessToken;
         // The signed-in user info.
         var user = result.user;
         // redirect to home page
         window.location("/home-page.html");
-      }).catch(function(error) {
+    }).catch(function (error) {
         // Handle Errors here.
         var errorCode = error.code;
         var errorMessage = error.message;
@@ -119,30 +125,9 @@ function OAuthSignIn() {
         console.log("Error Code\n" + errorCode + "\n\n")
         console.log("Error Email\n" + email + "\n\n")
         console.log("Error Credential\n" + credential + "\n\n")
+
+        $("#login-error").show();
         // ...
-      });
-
-
-    // firebase.auth().signInWithPopup(provider).then(function (result) {
-    //     // This gives you a Google Access Token. You can use it to access the Google API.
-    //     var token = result.credential.accessToken;
-    //     // The signed-in user info.
-    //     var user = result.user;
-    //     // redirect to home page
-    //     window.location("/home-page.html");
-    // }).catch(function (error) {
-    //     // Handle Errors here.
-    //     var errorCode = error.code;
-    //     var errorMessage = error.message;
-    //     // The email of the user's account used.
-    //     var email = error.email;
-    //     // The firebase.auth.AuthCredential type that was used.
-    //     var credential = error.credential;
-
-    //     
-
-    //     $("#login-error").show();
-    //     // ...
-    // });
+    });
 
 }
